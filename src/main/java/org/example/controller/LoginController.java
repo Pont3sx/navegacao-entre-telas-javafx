f9f61bdfcdc32c6926afd139fe2a6eb03da0351e
Samily Pontes <pontess.dev@gmail.com>
2026-05-19 22:13:30 -0300

    Primeiro commit

---
+++ src/main/java/org/example/controller/LoginController.java
@@ -0,0 +1,25 @@
+package org.example.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import javafx.event.ActionEvent;
+
+public class LoginController {
+    @FXML
+    public void entrar(ActionEvent evento) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/fxml/TelaLogin.fxml"));
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) ((Node)evento.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
